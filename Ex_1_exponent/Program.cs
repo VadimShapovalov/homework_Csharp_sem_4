d538d195f003c689e1786e3d41b01825fc2d029a
VadimShapovalov <kidav_21@mail.ru>
2022-08-04 21:43:43 +0300

    Возведение числа в степень

---
+++ Ex_1_exponent/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+Console.WriteLine("Введите число являющееся основанием степени: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число являющееся показателем степени: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+int result;
+for(result = a * a; count < b; count++)
+{
+    result = result * a;
+}
+Console.WriteLine($"{a} в степени {b} равно {result}");

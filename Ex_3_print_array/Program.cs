5b135ae0c7df96df884573791b4b5b6f80446a15
VadimShapovalov <kidav_21@mail.ru>
2022-08-04 22:00:54 +0300

    Ввод чисел через запятую и вывод массива через запятую

---
+++ Ex_3_print_array/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 29: (необязательная) Напишите программу, которая создает массив из введенных через запятую цифр и выводит этот массив на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+Console.WriteLine("Введите числа через запятую: ");
+string numbers = Console.ReadLine();
+int[] array = numbers.Split(',').Select(int.Parse).ToArray();
+Console.Write(string.Join(",", array));
+
